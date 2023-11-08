85d97ff965e237d09fcaef59cf9f48936e158d02
Chris Nuland <you@example.com>
2023-11-08 16:33:11 -0500

    update the code to upload to weaviate

--- apps/pop-queue.py
+++ apps/pop-queue.py
@@ -2,9 +2,39 @@ import asyncio
 from azure.servicebus.aio import ServiceBusClient
 from azure.servicebus import ServiceBusMessage
 from azure.identity.aio import DefaultAzureCredential
-
+import logging
+import json
 import sys, os
+import weaviate_utils
 
+logging.basicConfig(level=logging.INFO)
+
+client = weaviate_utils.weaviate_connection()
+logging.info('\nclient.isReady(): %s', client.is_ready())
+logging.info('cluster.get_nodes_status(): %s', client.cluster.get_nodes_status())
+
+class_obj = {
+    "class": "Question",
+    "vectorizer": "text2vec-huggingface",
+    "moduleConfig": {
+        "text2vec-huggingface": {},
+        "model": "sentence-transformers/all-MiniLM-L6-v2",
+        # Ensure the `generative-openai` module is used for generative queries
+        "generative-openai": {
+          "model": "gpt-3.5-turbo",  # Optional - Defaults to `gpt-3.5-turbo`
+        }
+    }
+}
+
+#
+# Create the class if its not there
+#
+
+if client.schema.exists('Question'):
+    logging.info("Question class already exists, skipping class creation.")
+else:
+    logging.info(f'\nCreating the Question class using the text2vec-huggingface vectorizer.')
+    client.schema.create_class(class_obj)
 QUEUE_NAME = "test"
 connection = os.getenv('CONNECTION_STRING')
 credential = DefaultAzureCredential()
@@ -22,9 +52,23 @@ async def run():
                     received_msgs = await receiver.receive_messages(max_wait_time=5, max_message_count=20)
                     for msg in received_msgs:
                         print("Received: " + str(msg))
+
+                        data = json.loads(msg)  # Load data
+
+                        client.batch.configure(batch_size=100)  # Configure batch
+                        with client.batch as batch:  # Initialize a batch process
+                            for i, d in enumerate(data):  # Batch import data
+                                print(f"importing question: {i+1}")
+                                properties = {
+                                    "answer": d["Answer"],
+                                    "question": d["Question"],
+                                    "category": d["Category"],
+                                }
+                                batch.add_data_object(
+                                    data_object=properties,
+                                    class_name="Question"
+                                )
                         # complete the message so that the message is removed from the queue
                         await receiver.complete_message(msg)
                         sys.exit()
-
-
 asyncio.run(run())
